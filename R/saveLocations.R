575c3d2c517256dbe8d9bb3034b0d87813941f80
AlbaSevilla <91958503+AlbaSevilla@users.noreply.github.com>
2026-05-27 13:57:25 +0200

    Delete R directory

--- R/saveLocations.R
+++
@@ -1,6 +0,0 @@
-saveLocations <- function(text) {
-  words <- unlist(strsplit(text, " "))
-  filter_words <- words[grepl("^[A-Z]", words)]
-  result <- paste(filter_words, collapse = " ")
-  return(result)
-}
